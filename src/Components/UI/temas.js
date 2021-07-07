098dcc9787eca498b01f519998d763e5d20fc725
Edson Primo <edson.oliveira00@hotmail.com>
2021-07-07 15:07:03 -0300

    curso finalizado

---
+++ src/Components/UI/temas.js
@@ -0,0 +1,15 @@
+import {fundoClaro, conteudoClaro, textoFundoClaro, fundoEscuro, conteudoEscuro, textoFundoEscuro} from './variaveis'
+
+export const temaClaro = {
+  body: fundoClaro,
+  inside: conteudoClaro,
+  texto: textoFundoClaro,
+  filter: ''
+};
+
+export const temaEscuro = {
+  body: fundoEscuro,
+  inside: conteudoEscuro,
+  texto: textoFundoEscuro,
+  filter: "invert(100%)",
+};
